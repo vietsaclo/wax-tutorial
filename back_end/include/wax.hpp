a2069dfb9a4deed9d893e91c94f26b91bafca504
vietsaclo <vietsaclo@gmail.com>
2021-07-13 14:35:49 +0700

    Back-end table

--- back_end/include/wax.hpp
+++ back_end/include/wax.hpp
@@ -1,4 +1,4 @@
-// Inherit your contract from eosio::contract. 
+// Inherit your contract from eosio::contract.
 // This exposes the following data types (available to your smart contract):
 // eosio::name receiver - the contract that receives an action (this contract)
 // eosio::name code - the contract's blockchain account
@@ -6,15 +6,38 @@
 #include <eosio/eosio.hpp>
 using namespace eosio;
 
-CONTRACT wax : public contract {
-   public:
-      using contract::contract;
+CONTRACT wax : public contract
+{
+public:
+  using contract::contract;
 
-      // The ACTION keyword implements the behavior of your contract. 
-      // ACTION is a shortcut for [[eosio::action]]  
-      ACTION hi( name nm );
+  // constructor
+  wax(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
+                                                               _tb_keys_values(receiver, receiver.value) {}
 
-      //action_wrapper: first parameter = action to call
-      //second parameter = pointer to the action function
-      using hi_action = action_wrapper<"hi"_n, &wax::hi>;
+  // The ACTION keyword implements the behavior of your contract.
+  // ACTION is a shortcut for [[eosio::action]]
+  ACTION hi(name nm);
+
+  //action_wrapper: first parameter = action to call
+  //second parameter = pointer to the action function
+  using hi_action = action_wrapper<"hi"_n, &wax::hi>;
+
+  //===== Action 2
+  ACTION helloworld(name text);
+  using helloWorld_action = action_wrapper<"helloworld"_n, &wax::helloworld>;
+
+  //===== Action 3
+  ACTION addkeyvalue(name username, name key, std::string value);
+  using addkeyvalue_action = action_wrapper<"addkeyvalue"_n, &wax::addkeyvalue>;
+
+private:
+  struct [[eosio::table]] tbkeysvalues
+  {
+    name key;
+    std::string value;
+    uint64_t primary_key() const { return key.value; }
+  };
+  typedef eosio::multi_index<name("tbkeysvalues"), tbkeysvalues> tb_keys_values;
+  tb_keys_values _tb_keys_values;
 };
